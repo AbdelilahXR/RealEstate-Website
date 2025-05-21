c68cf9eb5efb70f231c309e016afaf1a258b541e
Abdelilah <ettarch.abdelilah@gmail.com>
2025-05-21 21:12:23 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='absolute top-0 left-0 w-full z-10'>
+        <div>
+            <img src="" alt="" />
+        </div> 
+    </div>
+  )
+}
+
+export default Navbar
