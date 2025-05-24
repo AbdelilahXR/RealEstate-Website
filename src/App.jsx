57a5f93d23f0fe45e8b2ea1fd4a1fcb37c0e7a63
Abdelilah <ettarch.abdelilah@gmail.com>
2025-05-25 00:58:59 +0100

    About us

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import About from './components/About'
 
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <div className='w-full  overflow-hidden '>
          <Header/>
+         <About></About>
       </div>
      
     </>
