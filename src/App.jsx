d8ca90c3509b01b986b6050d1d163e3676e12207
Abdelilah <ettarch.abdelilah@gmail.com>
2025-05-27 22:59:31 +0100

    reviews secton update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
          <Header/>
          <About></About>
          <Projects></Projects>
+         <Testimonails></Testimonails>
       </div>
      
     </>
