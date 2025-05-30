0d9f103b7903c5c12be12faebf3bedfe04f96948
Abdelilah <ettarch.abdelilah@gmail.com>
2025-05-30 16:15:31 +0100

    updating contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,58 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "311c8bb4-ccd9-448f-8d7e-8a1242ac04d6");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully")
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message)
+      setResult("");
+    }
+  };
+
+
+
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full' id='Contact'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact 
             <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
         <p className='text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let’s Build Your Future Together</p>
 
-        <form action="">
-            <div>
-                <div>Your Name</div>
-                <input type="text" placeholder='Your Name'/>
+        <form onSubmit={onSubmit} className='max-w-2xl mx-auto '>
+            <div className='flex flex-wrap '>
+                <div className='w-full md:w-1/2 text-left'>
+                     Your Name
+                    <input type="text" placeholder='Your Name' name='Name' className='w-full border border-gray-300 py-3 px-4 mt-2 mb-3 rounded'/>
+                </div>
+                <div className='w-full md:w-1/2 text-left md:pl-4'>
+                    Your Email
+                    <input type="text" placeholder='Your Email' name='Email' className='w-full border border-gray-300 py-3 px-4 mt-2  rounded '/>
+                </div>
+                <div className='w-full text-left my-6'>
+                    Your Message    
+                    <textarea className='w-full border border-gray-300 rounded py-3 px-4 mt-2  h-48 resize-none' name="Message"  required></textarea>
+                </div>
             </div>
-            <div>
-                <div>Your Email</div>
-                <input type="text" placeholder='Your Email' />
-            </div>
-            <div>
-                <div>Message</div>
-                <input type="text" placeholder='Message' />
-            </div>
-            <button>Send Message</button>
+            <button className='bg-blue-600 text-white py-2 px-8 rounded cursor-pointer '>{result ? result : "Sending Message"}</button>
         </form>
       
     </div>
--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -6,7 +6,7 @@ const Testimonails = () => {
     <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '>Customer 
             <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonails</span></h1>
-        <p className='text-center text-gray-500 mb-8 max-w-80 mx-80'>Real Stories from Those Who Found Home with Us</p>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto '>Real Stories from Those Who Found Home with Us</p>
 
         <div className='flex flex-wrap justify-center gap-8'>
             {testimonialsData.map((testimonial, index)  =>(
